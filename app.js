e39396e76cd2b72786d674535b393f85d3abadb3
rofaba <97056237+rofaba@users.noreply.github.com>
2022-06-15 10:50:24 -0400

    entregable 4 tested

--- app.js
+++ app.js
@@ -11,27 +11,27 @@ const port = 8080;
 
 //middlewares
 app.use(express.json())
-app.use(express.urlencoded({extend: true}))
+app.use(express.urlencoded({ extend: true }))
 // app.use('/static', express.static(__dirname + '/public'));
 
 //rutas
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Servidor online. Favor dirigirse a "localhost:8080/api" para empezar')
 })
- 
+
 app.get('/api', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
-   
+
 })
 
 app.use('/api/productos', router);
 
 //error404
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.status(404).sendFile(__dirname + '/public/404.html')
 })
 //puerto activo
 app.listen(port, () => {
-        console.log(`El servidor se encuentra operativo y esperando solicitudes en el puerto ${port}`);
-    })
+    console.log(`El servidor se encuentra operativo y esperando solicitudes en el puerto ${port}`);
+})
--- routes.js
+++ routes.js
@@ -2,24 +2,24 @@ const express = require('express');
 const app = express();
 const { Router } = express;
 const productos = [
-  {
-    "title": "Funko Pop Star Wars: The Mandalorian",
-    "price": 24990,
-    "thumbnail": "https://m.media-amazon.com/images/I/5176rALHhgS._AC_UL480_FMwebp_QL65_.jpg",
-    "id": 1
-  },
-  {
-    "title": "Funko Pop Televisión: Silicon Valley Gilfoyle",
-    "price": 24990,
-    "thumbnail": "https://m.media-amazon.com/images/I/41PsLYv3r2L._AC_.jpg",
-    "id": 2
-  },
-  {
-    "title": "Funko Pop Star Wars: The Mandalorian",
-    "price": 24990,
-    "thumbnail": "https://m.media-amazon.com/images/I/51d9zjK3DdL._AC_SX466_.jpg",
-    "id": 3
-  }
+    {
+        "title": "Funko Pop Star Wars: The Mandalorian",
+        "price": 24990,
+        "thumbnail": "https://m.media-amazon.com/images/I/5176rALHhgS._AC_UL480_FMwebp_QL65_.jpg",
+        "id": 1
+    },
+    {
+        "title": "Funko Pop Televisión: Silicon Valley Gilfoyle",
+        "price": 24990,
+        "thumbnail": "https://m.media-amazon.com/images/I/41PsLYv3r2L._AC_.jpg",
+        "id": 2
+    },
+    {
+        "title": "Funko Pop Star Wars: The Mandalorian",
+        "price": 24990,
+        "thumbnail": "https://m.media-amazon.com/images/I/51d9zjK3DdL._AC_SX466_.jpg",
+        "id": 3
+    }
 ];
 const router = Router();
 
@@ -29,60 +29,60 @@ router.get('/', (req, res) => {
 
 router.get('/:num', (req, res) => {
     const posicion = Number(req.params.num);
-    console.log(posicion)
     const elementoBuscado = productos.find((element) => element.id == posicion);
-            if (elementoBuscado == undefined) {
-                res.json({error: "producto no encontrado"})
-                
-            } else {res.json(elementoBuscado)
-            }
+    if (elementoBuscado == undefined) {
+        res.json({ error: "producto no encontrado" })
+
+    } else {
+        res.json(elementoBuscado)
+    }
 })
 
 router.post('/', (req, res) => {
-        try { const nuevoProducto = req.body;
-        console.log(nuevoProducto)
-            if (productos.length == 0) { 
+    try {
+        const nuevoProducto = req.body;
+        if (productos.length == 0) {
             nuevoProducto.id = 1
-            } else {
-                const identificadores = [];
-                productos.forEach(element => identificadores.push(element.id));
-                nuevoProducto.id = (Math.max(...identificadores) + 1);
-            }
+        } else {
+            const identificadores = [];
+            productos.forEach(element => identificadores.push(element.id));
+            nuevoProducto.id = (Math.max(...identificadores) + 1);
+        }
         productos.push(nuevoProducto)
         res.json(nuevoProducto)
-        }
-        catch (error) {
-            console.log('Ha ocurrido un error en el proceso', error)
-        }
-})        
+    }
+    catch (error) {
+        console.log('Ha ocurrido un error en el proceso', error)
+    }
+})
 
 router.put('/:num', (req, res) => {
     const idBuscado = Number(req.params.num);
     const elementoBuscado = productos.find((element) => element.id == idBuscado);
-            if (elementoBuscado == undefined) {
-                res.json({error: "producto no encontrado"})
-                
-            } else {
-                elementoBuscado.title = req.body.title;
-                elementoBuscado.price = req.body.price;
-                elementoBuscado.thumbnail = req.body.thumbnail
-                res.json(elementoBuscado)
-            }
-                  
+    if (elementoBuscado == undefined) {
+        res.json({ error: "producto no encontrado" })
+
+    } else {
+        elementoBuscado.title = req.body.title;
+        elementoBuscado.price = req.body.price;
+        elementoBuscado.thumbnail = req.body.thumbnail
+        res.json(elementoBuscado)
+    }
+
 })
 
 router.delete('/:num', (req, res) => {
     const idBuscado = Number(req.params.num);
     const elementoBuscado = productos.find((element) => element.id == idBuscado);
-            if (elementoBuscado == undefined) {
-                res.json({error: "producto no encontrado"})
-                
-            } else {
-                const indexToDelete = productos.findIndex((element) => element.id == idBuscado)
-                productos.splice(indexToDelete,1)
-                console.log(productos)
-                res.send('Elemento Eliminado')
-            }
-        })           
+    if (elementoBuscado == undefined) {
+        res.json({ error: "producto no encontrado" })
+
+    } else {
+        const indexToDelete = productos.findIndex((element) => element.id == idBuscado)
+        productos.splice(indexToDelete, 1)
+        console.log(productos)
+        res.send('Elemento Eliminado')
+    }
+})
 
 module.exports = router;
